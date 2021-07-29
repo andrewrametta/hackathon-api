162a32f0f9653a2b9f8abfcd812f23c12193fe95
Andrew <ramettaandrewj@gmail.com>
2021-07-29 14:11:29 -0400

    connecting to db

--- src/config.js
+++ src/config.js
@@ -1,4 +1,11 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
+  JWT_SECRET: process.env.JWT_SECRET || "black-jack-saloon",
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://Metta@localhost/blackjack-saloon",
+  TEST_DATABASE_URL:
+    process.env.TEST_DATABASE_URL ||
+    "postgresql://Metta@localhost/blackjack-saloon",
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || "http://localhost:3000",
 };

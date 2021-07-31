6cdf9c4b11ddf453686822b4e2568e4642f7cbad
Andrew <ramettaandrewj@gmail.com>
2021-07-31 16:38:23 -0400

    use db

--- src/server.js
+++ src/server.js
@@ -1,5 +1,11 @@
 const knex = require("knex");
+const { socket } = require("socket.io");
 const app = require("./app");
+const io = require("socket.io")(4000, {
+  cors: {
+    origin: ["url"],
+  },
+});
 
 const { PORT, DATABASE_URL } = require("./config");
 
@@ -8,6 +14,31 @@ const db = knex({
   connection: DATABASE_URL,
 });
 
+app.set("db", db);
+
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
+});
+
+//set max connections for room to 5
+const connections = [null, null, null, null, null];
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+  // find an available player
+  // let playerIndex = -1
+  // for (const i in connections) {
+  //   if(connections[i]=== null){
+  //     playerIndex = i
+  //     break
+  //   }
+  // }
+  // Ignore player 6
+  //if (playerIndex === -1) return
+  // Tell the conncecting player what player number they are
+  // socket.emit('player-number', playerIndex)
+  // console.log(`Player ${playerIndex} has connected`)
+  socket.on("custom-event", (params) => {
+    console.log(params);
+  });
 });

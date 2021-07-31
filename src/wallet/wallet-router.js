ecd2231ef24248c0cc4c29f2973119d62bba6945
Andrew <ramettaandrewj@gmail.com>
2021-07-31 15:40:10 -0400

    added post wallet route

--- src/wallet/wallet-router.js
+++ src/wallet/wallet-router.js
@@ -11,13 +11,34 @@ const serializeWallet = (wallet) => ({
   total: xss(wallet.total),
 });
 
-walletRouter.route("/").get(requireAuth, (req, res, next) => {
-  const knexInstance = req.app.get("db");
+walletRouter
+  .route("/")
+  .get(requireAuth, (req, res, next) => {
+    const knexInstance = req.app.get("db");
 
-  const user_id = req.user.id;
-  WalletService.getWalletByUserId(knexInstance, user_id)
-    .then((wallet) => {
-      res.json(wallet.map(serializeWallet));
-    })
-    .catch(next);
-});
+    const user_id = req.user.id;
+    WalletService.getWalletByUserId(knexInstance, user_id)
+      .then((wallet) => {
+        res.json(wallet.map(serializeWallet));
+      })
+      .catch(next);
+  })
+  .post(requireAuth, (req, res, next) => {
+    const { total } = req.body;
+    const newWallet = { total };
+
+    for (const [key, value] of Object.entries(newWallet))
+      if (value == null)
+        return res.status(400).json({
+          error: { message: `'${key}' is required` },
+        });
+    newWallet.user_id = req.user.id;
+    WalletService.insertWallet(req.app.get("db"), newWallet)
+      .then((wallet) => {
+        res
+          .status(201)
+          .location(`/wallet/${wallet.id}`)
+          .json(serializeItem(wallet));
+      })
+      .catch(next);
+  });

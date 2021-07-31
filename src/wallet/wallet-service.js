358a4df1f8ef21a6164ca298d524b80594305f00
Andrew <ramettaandrewj@gmail.com>
2021-07-31 15:18:32 -0400

    add get wallets with users table join

--- src/wallet/wallet-service.js
+++ src/wallet/wallet-service.js
@@ -1,4 +1,11 @@
 const WalletService = {
+  getAllWallets(knex) {
+    return knex
+      .select(knex.raw("wallet.*, users.username, users.user_url"))
+      .join("users", { "users.id": "wallet.user_id" })
+      .from("wallet");
+  },
+
   getWalletByUserId(knex, user_id) {
     return knex.from("wallet").select("*").where("user_id", user_id).first();
   },
